69a0ecac2f834969acc6417d04e86c242cdec329
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2022-11-03 06:34:25 -0700

    make line chart compatible with mobile

--- projects/plastic-on-earth/js/d3/global-plastic-production.js
+++ projects/plastic-on-earth/js/d3/global-plastic-production.js
@@ -6,7 +6,6 @@ function main() {
 
     d3.csv("data/global-plastics-production.csv").then(
         (data) => {
-            console.log(data);
             data.forEach(function(d) {
                 d.Amount = +d["Global plastics production"];
             });
@@ -21,44 +20,54 @@ function main() {
 
 function lineChart(data) {
 
+    let isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        isMobile = true;
+    }
+
     // Setting up canvas width and height
-    const canvasWidth = 700;
-    const canvasHeight = 600;
+    let width = $('#graph').width();
+    let height = isMobile? $('#graph').height() * 1.7 : $('#graph').height() * 3;
+
     // Define all the margins within canvas
-    const marginLeft = canvasWidth * 0.2;
-    const marginRight = canvasWidth * 0.1;
-    const marginTop = canvasHeight * 0.15;
-    const marginBot = canvasHeight * 0.2;
+    const marginLeft = width * 0.2;
+    const marginRight = width * 0.1;
+    const marginTop = height * 0.15;
+    const marginBot = height * 0.2;
+
+    let element = document.getElementsByTagName("h3");
+    let style = window.getComputedStyle(element[0], null).getPropertyValue('font-size');
+    let fontSize = parseFloat(style);
 
     //let svg = d3.select("#graph #test").append("svg")
     let svg = d3.select("svg")
-    svg.attr("width", canvasWidth)
-        .attr("height", canvasHeight);
+    svg.attr("width", width)
+        .attr("height", height)
 
     let minMaxYear = d3.extent(data, function(d) { return d.Year; })
     // Scale and add X axis
     let xScale = d3.scaleTime()
         .domain([new Date(minMaxYear[0], 0, 1), new Date(minMaxYear[1], 0, 1)])
-        .range([ 0, canvasWidth - marginLeft - marginRight ]);
-    svg.append("g")
-        .attr("transform", `translate(${marginLeft}, ${canvasHeight - marginBot})`)
+        .range([ 0, width - marginLeft - marginRight ]);
+    let xAxis = svg.append("g")
+        .attr("transform", `translate(${marginLeft}, ${height - marginBot})`)
         .call(d3.axisBottom(xScale)) // syntax: d3.axisLeft(scale) - construct an y-axis for the given scale
-        .selectAll("text") // select all the text element
-
+    
     // Scale and add Y axis
     let yScale = d3.scaleLinear()
         .domain([0, d3.max(data, function(d) { return d.Amount; })]) // actual number on data
-        .range([canvasHeight - marginTop - marginBot, 0]); // available dimension on canvas
-    svg.append("g")
+        .range([height - marginTop - marginBot, 0]); // available dimension on canvas
+    let yAxis = svg.append("g")
         .attr("transform", `translate(${marginLeft}, ${marginTop})`)
-        .call(d3.axisLeft(yScale)); // syntax: d3.axisLeft(scale) - construct an y-axis for the given scale
-
+        .call(d3.axisLeft(yScale)) // syntax: d3.axisLeft(scale) - construct an y-axis for the given scale
 
     // Define line generator
     // The x and y accessors tell the line generator how to decide where to place each point on the line.
     let line = d3.line()
-        .x(function(d) { console.log(d.Year); return marginLeft + xScale(new Date(d.Year, 0, 1)); })
-        .y(function(d) { console.log(d.Amount); return marginTop + yScale(Math.abs(d.Amount)); });
+        .x(function(d) { return marginLeft + xScale(new Date(d.Year, 0, 1)); })
+        .y(function(d) { return marginTop + yScale(Math.abs(d.Amount)); });
 
     //Create line
     svg.append("path")
@@ -71,24 +80,37 @@ function lineChart(data) {
         .attr("d", line);
 
     // Add X axis title
-    svg.append("text")
+    let xTitle = svg.append("text")
         .text("Years")
-        .attr("x", (marginLeft + (canvasWidth - marginRight - marginLeft) / 2))
-        .attr("y", canvasHeight * 0.95)
-        .attr("font-size", "20")
+        .attr("x", (marginLeft + (width - marginRight - marginLeft) / 2))
+        .attr("y", height * 0.95)
+        // .attr("font-size", fontSize)
         .attr("fill", "white")
         .style("text-anchor", "middle")
 
     // Add Y axis title
-    svg.append("text")
+    let yTitle = svg.append("text")
         .text("Plastic Amount (Ton)")
-        .attr("font-size", "20")
         .attr("transform",
-            `translate(${canvasWidth * 0.03}, ${marginTop + (canvasHeight - marginBot - marginTop) / 2}) 
+            `translate(${width * 0.03}, ${marginTop + (height - marginBot - marginTop) / 2}) 
             rotate(-90)`)
+        // .attr("font-size", fontSize)
         .attr("fill", "white")
         .style("text-anchor", "middle")
 
+    if (isMobile) {
+        xAxis.selectAll("text") // select all the text element
+        .attr("font-size", fontSize - 12);
+
+        yAxis.selectAll("text") // select all the text element
+        .attr("font-size", fontSize - 12);
+
+        xTitle.selectAll("text")
+        .attr("font-size", fontSize - 12);
+
+        yTitle.selectAll("text")
+        .attr("font-size", fontSize - 12);
+    }
 
 }
 
